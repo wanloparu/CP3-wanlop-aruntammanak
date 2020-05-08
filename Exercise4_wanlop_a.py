d1f7ec17f57f7032c4fad29eec91e08f8707d18c
wanloparu <wanlop.aru@rmutr.ac.th>
2020-05-08 22:30:03 +0700

    Exercise4_wanlop_a
    Exercise4_wanlop_a

---
+++ Exercise4_wanlop_a.py
@@ -0,0 +1,9 @@
+FoundationEnglish = 80
+GeneralBusiness = 60
+IntroductiontoComputerSystems = 87
+ComputerProgramming = 89
+print("--- Your Score ---")
+print("FoundationEnglish :",float(FoundationEnglish))
+print("GeneralBusiness :",float(GeneralBusiness))
+print("IntroductiontoComputerSystems :",float(IntroductiontoComputerSystems))
+print("ComputerProgramming :",float(ComputerProgramming))
