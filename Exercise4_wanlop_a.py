4e8095f979af5222b9355d9667dd235b18ddb190
wanloparu <wanlop.aru@rmutr.ac.th>
2022-04-25 13:28:43 +0700

    final

--- Exercise4_wanlop_a.py
+++ Exercise4_wanlop_a.py
@@ -3,7 +3,7 @@ GeneralBusiness = 60
 IntroductiontoComputerSystems = 87
 ComputerProgramming = 89
 print("--- Your Score ---")
-print("FoundationEnglish :",float(FoundationEnglish))
-print("GeneralBusiness :",float(GeneralBusiness))
-print("IntroductiontoComputerSystems :",float(IntroductiontoComputerSystems))
-print("ComputerProgramming :",float(ComputerProgramming))
+print("FoundationEnglish :", float(FoundationEnglish))
+print("GeneralBusiness :", float(GeneralBusiness))
+print("IntroductiontoComputerSystems :", float(IntroductiontoComputerSystems))
+print("ComputerProgramming :", float(ComputerProgramming))
